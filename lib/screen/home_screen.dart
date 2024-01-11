b76b318340df1547ce1d6e0861512a6fb22adfcb
Javed Khan <Mohammad Javed Khan>
2024-01-11 12:29:19 +0530

    user details wip

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:exhibition_data_collection/screen/user_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,124 +17,119 @@ class _HomeScreenState extends State<HomeScreen> {
     TabList(name: 'tab 2', isSelected: false, answer: 'ans 2'),
     TabList(name: 'tab 3', isSelected: false, answer: 'ans 3'),
     TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
+    TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
+    TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
+    TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
+    TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
+    TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
+    TabList(name: 'tab 4', isSelected: false, answer: 'ans 4'),
   ];
   final TextEditingController _otherFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: InkWell(
-          onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: MediaQuery.sizeOf(context).height / 2,
-                    child: GridView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: _tabList.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 8,
-                        crossAxisSpacing: 8,
-                      ),
-                      itemBuilder: (BuildContext context, int index) {
-                        return InkWell(
-                          onTap: () {
-                            setState(() {});
-                            if (_tabList[index].isSelected) {
-                              _tabList[index].isSelected = false;
-                            } else {
-                              _tabList[index].isSelected = true;
-                            }
-                          },
-                          child: Container(
-                            height: MediaQuery.sizeOf(context).height / 4,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: _tabList[index].isSelected
-                                  ? Colors.green
-                                  : Colors.red,
-                            ),
-                            child: Center(
-                              child: Text(
-                                _tabList[index].name,
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+    return Scaffold(
+      body: InkWell(
+        onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height / 1.8,
+                child: GridView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: _tabList.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
                   ),
-                  SizedBox(height: MediaQuery.sizeOf(context).height / 100),
-                  InkWell(
-                    onTap: () {
-                      setState(() {});
-                      _isOtherTabSelected = !_isOtherTabSelected;
-                    },
-                    child: Container(
-                      height: MediaQuery.sizeOf(context).height / 15,
-                      width: MediaQuery.sizeOf(context).width / 1.5,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: _isOtherTabSelected ? Colors.green : Colors.red,
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'Other',
-                          style: TextStyle(color: Colors.white),
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        setState(() {});
+                        if (_tabList[index].isSelected) {
+                          _tabList[index].isSelected = false;
+                        } else {
+                          _tabList[index].isSelected = true;
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: _tabList[index].isSelected
+                              ? Colors.green
+                              : Colors.red,
+                        ),
+                        child: Center(
+                          child: Text(
+                            _tabList[index].name,
+                            style: const TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height / 50),
+              InkWell(
+                onTap: () {
+                  setState(() {});
+                  _isOtherTabSelected = !_isOtherTabSelected;
+                },
+                child: Container(
+                  height: MediaQuery.sizeOf(context).height / 15,
+                  width: MediaQuery.sizeOf(context).width / 1.5,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: _isOtherTabSelected ? Colors.green : Colors.red,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Other',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  SizedBox(height: MediaQuery.sizeOf(context).height / 100),
-                  _isOtherTabSelected
-                      ? SizedBox(
-                          width: MediaQuery.sizeOf(context).width,
-                          child: TextField(
-                            maxLines: 2,
-                            decoration: const InputDecoration(
-                              hintText: 'Other Field',
-                              border: OutlineInputBorder(),
-                            ),
-                            controller: _otherFieldController,
-                          ),
-                        )
-                      : const SizedBox(),
-                  SizedBox(height: MediaQuery.sizeOf(context).height / 7),
-                ],
+                ),
               ),
-            ),
+              SizedBox(height: MediaQuery.sizeOf(context).height / 50),
+              _isOtherTabSelected
+                  ? TextField(
+                      decoration: const InputDecoration(
+                        hintText: 'Other Field',
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: _otherFieldController,
+                    )
+                  : const SizedBox(),
+              SizedBox(height: MediaQuery.sizeOf(context).height / 7),
+            ],
           ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: Container(
-          padding: const EdgeInsets.only(bottom: 25),
-          width: MediaQuery.sizeOf(context).width / 2,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
-            onPressed: () {
-              //TODO submit data on server or any storage
-
-              var data = submitData();
-              log('data List ${data.toString()}');
-            },
-            child: const Text(
-              'Submit',
-              style: TextStyle(color: Colors.white),
-            ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.only(bottom: 25),
+        width: MediaQuery.sizeOf(context).width / 2,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+          onPressed: () {
+            //TODO submit data on server or any storage
+            var data = _submitUserSelectedData();
+            log('data List ${data.toString()}');
+          },
+          child: const Text(
+            'Submit',
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
     );
   }
 
-  List<String> submitData() {
+  List<String> _submitUserSelectedData() {
     List<String> dataList = [];
     for (int i = 0; i < _tabList.length; i++) {
       if (_tabList[i].isSelected) {
@@ -159,6 +155,12 @@ class _HomeScreenState extends State<HomeScreen> {
           });
     } else {
       // TODO Navigation
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const UserDetailScreen(),
+        ),
+      );
     }
     return dataList;
   }
